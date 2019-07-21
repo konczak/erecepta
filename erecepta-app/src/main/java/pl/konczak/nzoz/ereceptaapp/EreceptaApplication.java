4fdc834dc0cf610cd5203aafc548d1b64232c359
konczak <konczak.piotrek@gmail.com>
2019-07-21 13:20:31 +0200

    add erecepta-app module
    - make module Spring boot powered,
    - configure project to be Web app,
    - configure CXF to create client for CSIOZ WS,
    - provide sample usage of CSIOZ WS client (it works on application start).

---
+++ erecepta-app/src/main/java/pl/konczak/nzoz/ereceptaapp/EreceptaApplication.java
@@ -0,0 +1,28 @@
+package pl.konczak.nzoz.ereceptaapp;
+
+import pl.gov.csioz.p1.erecepta.ws.v20170510.ObslugaReceptyWS;
+import pl.gov.csioz.p1.erecepta.ws.v20170510.ZapisPakietuReceptRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource("classpath:cxf.xml")
+public class EreceptaApplication
+        implements CommandLineRunner {
+
+    @Autowired
+    private ObslugaReceptyWS obslugaReceptyWSclient;
+
+    public static void main(String[] args) {
+        SpringApplication.run(EreceptaApplication.class, args);
+    }
+
+
+    public void run(final String... args) throws Exception {
+        obslugaReceptyWSclient.zapisPakietuRecept(new ZapisPakietuReceptRequest());
+    }
+}
