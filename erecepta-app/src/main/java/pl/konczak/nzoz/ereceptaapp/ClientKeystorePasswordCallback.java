fdbe4effe7299a4db2aa236c384fad9c98f53eb8
konczak <konczak.piotrek@gmail.com>
2019-07-21 19:59:41 +0200

    configure request signature using client certificate for data exchange
    - add missing Spring bus bean to config via import cxf/cxf.xml,
    - define namespace prefix "wsu",
    - add WSS4jOutInterceptor which creates Signature from body and kontekstWywolania header. Also it uses DirectReference as KeyIdentifier (to include public key in request)
    
    NOTE: looks that server understoods security elements and now fails on parse kontekstWywolania due to missing content.

---
+++ erecepta-app/src/main/java/pl/konczak/nzoz/ereceptaapp/ClientKeystorePasswordCallback.java
@@ -0,0 +1,24 @@
+package pl.konczak.nzoz.ereceptaapp;
+
+import org.apache.wss4j.common.ext.WSPasswordCallback;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.CallbackHandler;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import java.io.IOException;
+
+public class ClientKeystorePasswordCallback
+        implements CallbackHandler {
+
+    public ClientKeystorePasswordCallback() {
+        // WSHandlerConstants.
+    }
+
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        WSPasswordCallback pc = (WSPasswordCallback) callbacks[0];
+
+        // set the password for our message.
+        pc.setPassword("UXG9DxASCm");
+    }
+}
+
