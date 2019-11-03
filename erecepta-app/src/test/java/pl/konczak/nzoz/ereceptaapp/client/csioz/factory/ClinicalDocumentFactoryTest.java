4cc4944c2d60f0fbd36911a904a5d198fce7a177
konczak <konczak.piotrek@gmail.com>
2019-11-03 19:13:53 +0100

    add test for serialized CLinicalDocument compliant with plCdaReimbursementConformantDrugPrescription
    - test fails atm

---
+++ erecepta-app/src/test/java/pl/konczak/nzoz/ereceptaapp/client/csioz/factory/ClinicalDocumentFactoryTest.java
@@ -0,0 +1,61 @@
+package pl.konczak.nzoz.ereceptaapp.client.csioz.factory;
+
+import pl.gov.csioz.xsd.extpl.r2.ClinicalDocument;
+import pl.konczak.nzoz.ereceptaapp.util.SampleCreateEReceptaDlaLekGotowyInputFactory;
+import pl.konczak.nzoz.ereceptaapp.client.csioz.input.CreateEReceptaDlaLekGotowyInput;
+import pl.konczak.nzoz.ereceptaapp.util.XmlSerializer;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class ClinicalDocumentFactoryTest {
+
+    @Autowired
+    private ClinicalDocumentFactory clinicalDocumentFactory;
+
+    @Autowired
+    private SampleCreateEReceptaDlaLekGotowyInputFactory sampleCreateEReceptaDlaLekGotowyInputFactory;
+
+    @Autowired
+    private XmlSerializer xmlSerializer;
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void shouldReturnSerializedClinicalDocumentEqualsToExpectedWhenMarshall() throws Exception {
+        // given
+        CreateEReceptaDlaLekGotowyInput createEReceptaDlaLekGotowyInput = sampleCreateEReceptaDlaLekGotowyInputFactory.createSampleCreateEReceptaDlaLekGotowyInput();
+
+        ClinicalDocument clinicalDocument = clinicalDocumentFactory.createEReceptaDlaLekGotowy(createEReceptaDlaLekGotowyInput);
+
+        // when
+        String result = xmlSerializer.clinicalDocumentToXml(clinicalDocument);
+
+        // then
+        Path path = Paths.get(ClassLoader.getSystemResource("pl/konczak/nzoz/ereceptaapp/client/csioz/factory/sample-plCdaReimbursementConformantDrugPrescription-1.xml").toURI());
+        byte[] bytes = Files.readAllBytes(path);
+        String expectedContent = new String(bytes);
+
+        assertThat(result)
+                .isNotNull()
+                .isEqualTo(expectedContent);
+    }
+
+}
